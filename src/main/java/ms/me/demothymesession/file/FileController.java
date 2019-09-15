327648d3c0b5064c5c074a14be35b62a451ecfc1
mansoo <mac@msune-2.local>
2019-09-16 00:28:16 +0900

    파일다운로드

--- src/main/java/ms/me/demothymesession/file/FileController.java
+++ src/main/java/ms/me/demothymesession/file/FileController.java
@@ -1,17 +1,28 @@
 package ms.me.demothymesession.file;
 
+import com.sun.net.httpserver.Headers;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/files")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final ResourceLoader resourceLoader;
+
+
 
     @GetMapping("")
     public String fileHome(Model model){
@@ -25,5 +36,17 @@ public class FileController {
         attributes.addFlashAttribute("fileName", filename);
 
         return "redirect:/files";
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws IOException {
+        final Resource resource = resourceLoader.getResource("classpath:" + fileName);
+        File file = resource.getFile();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, "image/png")
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                .body(resource);
     }
 }
--- src/test/java/ms/me/demothymesession/file/FileControllerTest.java
+++ src/test/java/ms/me/demothymesession/file/FileControllerTest.java
@@ -4,13 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
